3a0b2f69518c24ef6edd96a93f13fbf73d76c6f9
Alex Do <ado@groupon.com>
2013-09-09 11:09:53 -0700

    Initial Commit

---
+++ DropdownKitkatz/dropdownAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  dropdownAppDelegate.h
+//  DropdownKitkatz
+//
+//  Created by Alex Do on 9/9/13.
+//  Copyright (c) 2013 Alex Do. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface dropdownAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DropdownKitkatz/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DropdownKitkatz
+//
+//  Created by Alex Do on 9/9/13.
+//  Copyright (c) 2013 Alex Do. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "dropdownAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([dropdownAppDelegate class]));
+  }
+}
